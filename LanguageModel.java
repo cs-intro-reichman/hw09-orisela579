98fe022e3115ee9dcd8cfd1268c1483e65bfaa4b
Ori Sela <orisela579@gmail.com>
2026-01-19 20:34:25 +0200

    :(

--- LanguageModel.java
+++ LanguageModel.java
@@ -97,7 +97,7 @@ public class LanguageModel {
         }
         String generatedText = initialText;
         String window = generatedText.substring(generatedText.length() - windowLength);
-        while (generatedText.length() < textLength) {
+        while (generatedText.length() < textLength + initialText.length()) {
             List probs = CharDataMap.get(window);
             if(probs == null){
                 break;
